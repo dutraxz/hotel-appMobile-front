663b4adaba130f7ef0bbd191d2c37843670c5632
dutraxz <victoreduardo0896@gmail.com>
2025-11-13 17:32:06 -0300

    feat | login: adicionar campos de entrada e botão no componente RenderLogin

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,7 +1,9 @@
 import TextField from "../ui/TextField";
 import AuthContainer from "../ui/AuthContainer";
-
-
+import PasswordField from "../ui/PasswordField";
+import { TouchableOpacity } from "react-native";
+import { global } from "../ui/styles";
+import { Text } from "@react-navigation/elements";
 
 const RenderLogin = () => {
     return (
@@ -10,12 +12,26 @@ const RenderLogin = () => {
             subtitle="Faça seu login para continuar!"
             icon="hotel">
 
-        <TextField
-            label="E-mail"
-            icon={"email"}>
-        </TextField>
+            {/* children */}   
+            <TextField
+                label="E-mail"
+                icon="email"
+                placeholder="user@gmail.com"
+                keyboardType="email-address"
+            />
+            
+            <PasswordField
+            label="Senha"
+            icon="lock"
+            placeholder="*********"
+            />
+
+        <TouchableOpacity style={[global.primaryButton]}>
+            <Text style={global.primaryButtonText}>Entrar</Text>
+        </TouchableOpacity>
+
         </AuthContainer>
     )
-}
+};
 
 export default RenderLogin;
