8cef9dd6002753d67cbeb983eb66496f7d68e97b
dutraxz <victoreduardo0896@gmail.com>
2025-11-19 17:02:15 -0300

    feat | room-card: adicionar componente RenderRoomCard para exibir informações da suíte

---
+++ src/components/ui/RenderRoomCard.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Image } from "react-native";
+import { global } from "./styles";
+
+const RenderRoomCard = () => {
+  return (
+    <View style={global.card}>
+
+      <Image
+        source={{uri: "https://midias.agazeta.com.br/2023/03/30/max-mello---suite-dos-noivos--1482020-article.jpg" }}
+        style={global.image}
+              />
+
+      <View style={global.infoSection}>
+        <Text style={global.titleCard}>Suite Victor
+      
+        </Text>
+      
+        
+
+        <Text style={global.price}>
+          R$ 150 por 1 noites
+        </Text>
+      </View>
+
+    </View>
+  );
+}
+export default RenderRoomCard;
