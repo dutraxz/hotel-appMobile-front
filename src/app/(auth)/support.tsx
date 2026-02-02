75e1e01270e9308ded7f2277287e1d29d40b9888
dutraxz <victoreduardo0896@gmail.com>
2026-02-02 17:33:35 -0300

    feat | Adicionar componente Support: exibir informações de contato para suporte

---
+++ src/app/(auth)/support.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+
+const Support = () => {
+    return (
+
+        <Text style={{color: "rgb(2, 24, 148)", fontSize: 14, marginTop: 100, textAlign: "center"}}>
+            Fale com o suporte - hotelroyal@gmail.com</Text>
+
+        
+    );
+}
+export default Support;
