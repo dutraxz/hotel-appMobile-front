c83016b5711660186b8df3b373435cfbf071364b
dutraxz <victoreduardo0896@gmail.com>
2026-02-06 17:33:59 -0300

    feat | Adicionar funcionalidade de seleção de datas para check-in e check-out, e implementar ScrollView para exibição de quartos

--- src/components/explorer/index.tsx
+++ src/components/explorer/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Dimensions, TouchableOpacity, View, Text, Pressable, Modal } from "react-native";
+import { Dimensions, TouchableOpacity, View, Text, Pressable, Modal, ScrollView } from "react-native";
 import AuthContainer from "../ui/AuthContainer";
 import DateSelector from "../ui/DateSelector";
 import RoomCard from "../ui/RoomCard";
 import TextField from "../ui/TextField";
 import InputSpin from "../ui/InputSpinner";
 import { global } from "../ui/styles";
+
+
 const RenderExplorer = () => {
   const { width, height } = Dimensions.get("window");
   //useState() para gerenciar e alterar os estados
@@ -17,58 +19,47 @@ const RenderExplorer = () => {
   
   return (
     <AuthContainer>
-      {/*children */}
-      <View style={{ display: "flex", /* flexDirection: "row", gap: width * 0.05,*/ justifyContent: "center" }}>
-        {/*Essa View vocês tinham e eu só estilizei*/}
-        <View style={{ display: "flex", flexDirection: "column" }}>                                              {/*Criei esta nova View para check-in*/}
+      <View style={{ display: "flex", justifyContent: "center" }}>
+        
+        {/* CHECK-IN */}
+
+        <View style={{ display: "flex", flexDirection: "column" }}>                                              
           {/* Input de checkIn para abrir calendário*/}
           <TouchableOpacity onPress={() => setCalendar("checkin")}>
-            <View style={{ width: width * 0.8 }}>  {/* 80% da tela para a área input*/}
-              {/* Nova view para dar largura ao TextField */}
+
+            <View style={{ width: width * 0.80 }}>  {/* 80% da tela para a área input*/}
+              
               <TextField
                 label="Check-in"
                 icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
                 placeholder="Selecione a data"
                 value={checkIn}
+                editable={false}
               />
-            </View> {""}
-            {/* Fecha aqui */}                                                                           {/* Fecha aqui */}
+            </View>                                                                          
           </TouchableOpacity>
-          {/* <DateSelector />
-          {calendar === "checkin" && (
-            <DateSelector onSelectDate={(date) => {
-              setCheckIn(date);
-              setCalendar(null);
-            }}
-            /> */}
-          {/* )} */}
-        </View> {""}
-                                                                                                          {/*View de check-in fecha aqui */}
-        {/*View de check-in fecha aqui */}
-        <View style={{ display: "flex", flexDirection: "column" }}>                                              {/*Criei esta nova View para check-out*/}
-          {" "}
-          {/*Criei esta nova View para check-out*/}
+        </View> 
+
+
+        {/* CHECK-OUT */}
+        <View style={{ display: "flex", flexDirection: "column" }}>
+
           {/* Input de checkIn para abrir calendário*/}
           <TouchableOpacity onPress={() => setCalendar("checkout")}>
-            <View style={{ width: width * 0.8 }}>  {/* 80% da tela para a área input*/}                            {/* Nova view para dar largura ao TextField */}
+
+            <View style={{ width: width * 0.8 }}>  {/* 80% da tela para a área input*/}
+
               <TextField
-                label="Check-out" icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
+                label="Check-out"
+                icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
                 placeholder="Selecione a data"
                 value={checkOut}
+                editable={false}
               />
             </View>
-            {/* Fecha aqui */}                                                                                             {/* Fecha aqui */}
           </TouchableOpacity>
-          {/* <DateSelector />
-          {calendar === "checkout" && (
-            // <DateSelector onSelectDate={(date) => {
-            //   setCheckOut(date);
-            //   setCalendar(null)
-            // }}
-            // />
-          )} */}
         </View>
-        {/*View do check-out que fecha aqui */}
+
 
         <Modal
           transparent
@@ -77,6 +68,7 @@ const RenderExplorer = () => {
           onRequestClose={closeCalendar}
         >
           {/*Backdrop: qualquer clique aqui fora, fecha*/}
+
           <Pressable
             style={{
               flex: 1,
@@ -84,12 +76,14 @@ const RenderExplorer = () => {
               alignItems: "center",
               backgroundColor: "rgba(0,0,0, 0.29)",
             }}
+
             onPress={closeCalendar}
           >
             {/*Area do calendario que ao clicar, nao o fecha */}
 
             <Pressable onPress={() => {}}>
               {/* <DateSelector /> */}
+
               {calendar === "checkin" && (
                 <DateSelector
                   onSelectDate={(date) => {
@@ -98,6 +92,7 @@ const RenderExplorer = () => {
                   }}
                 />
               )}
+
               {/* <DateSelector /> */}
               {calendar === "checkout" && (
                 <DateSelector
@@ -107,29 +102,53 @@ const RenderExplorer = () => {
                   }}
                 />
               )}
+
             </Pressable>
           </Pressable>
+
         </Modal>
-        {/* InputSpin */}                                                                                               {/*View do check-out que fecha aqui */}
+        {/* Quantidade de hospedes */}
+
         <View>
           <Text style={global.label}>Quantidade de Hospedes</Text>
-          {/*InputSpin*/}
+
           <InputSpin
             guests={qntGuests}
             onSelectSpin={(guests) => {
               setQntGuests(guests);
             }}
+
             minGuests={1}
             maxGuests={6}
             step={1}
             colorMin={"#051566ff"}
             colorMax={"#051566ff"}
           />
+
         </View>
       </View>
+
+      <View style={{ flex: 1, marginTop: height * 0.02, alignItems: "center" }}>
+
+
+      <ScrollView
+
+          contentContainerStyle={{ 
+
+            paddingLeft: 0 * 0.03,
+
+            paddingRight: 3, 
+
+            paddingTop: 3, 
+
+            paddingBottom: 5 
+
+          }}>
+
+          {Array.from({ length: 3 }).map((_, index) => (
+
       <RoomCard
         image={require("../../../assets/images/quartobranco.jpg")}
-        /* image={{uri: "https://"}} */
         label="Apartamento"
         icon={{
           lib: "FontAwesome5",
@@ -141,6 +160,13 @@ const RenderExplorer = () => {
           price: 180.90
         }}
       />
+      ))}
+
+
+      
+      
+      </ScrollView>
+      </View>
     </AuthContainer>
   );
 };
