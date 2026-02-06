2148bf655109af1a1240d9dcd2c4040242cc665e
dutraxz <victoreduardo0896@gmail.com>
2026-02-06 17:34:30 -0300

    refactor | Remover código comentado e limpar o componente DateSelector

--- src/components/ui/DateSelector.tsx
+++ src/components/ui/DateSelector.tsx
@@ -22,7 +22,6 @@ const DateSelector = ({ onSelectDate }: Props) => {
           borderColor: "#ffffffff", //borda
           textFontSize: 10,
           textHeaderFontSize: 15
-      
       }}
         style={{ borderRadius: 15, width: width * 0.65, height: "auto" }}
         isGregorian={true}
@@ -35,49 +34,3 @@ const DateSelector = ({ onSelectDate }: Props) => {
   );
 };;
 export default DateSelector;
-// const [open, setOpen] = useState(false);
-// const [date, setDate] = useState('2023/02/08');
-
-// const today = new Date();
-// const tomorrow = new Date(today);
-// tomorrow.setDate(today.getDate() + 1);
-
-// const startDate = getFormatedDate(tomorrow, "DD/MM/YYYY");
-
-
-// function handleOnPress() {
-//     setOpen(!open);
-// }
-
-//  function handleChange(selectedDate: string) {
-//     setDate(selectedDate);
-//  }
-
-//   return (
-//     <View>
-//         <TouchableOpacity onPress={handleOnPress}> 
-//             <Text>Abrir</Text>
-//         </TouchableOpacity>
-
-//      <Modal animationType="slide" transparent={true} visible={open}>
-//         <View style={global.centerView}>
-//           <View style={global.modalView}>
-//             <DatePicker
-//               mode="calendar"
-//               isGregorian={true}
-//               selected={date}
-//               minimumDate={startDate}
-//               onSelectedChange={handleChange}
-//             />
-
-//             <TouchableOpacity onPress={handleOnPress}>
-//               <Text>Close</Text>
-//             </TouchableOpacity>
-//           </View>
-//         </View>
-//       </Modal>
-//     </View>
-//   );
-// };
-
-// export default RenderDatePicker;
