6e60dc1f7f4c4b5cf306ac74b1dfd51db172677a
dutraxz <victoreduardo0896@gmail.com>
2026-02-06 17:33:41 -0300

    feat | Descomentar a tela de Login no layout de autentificação

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -9,7 +9,7 @@ import { Stack } from "expo-router";
 const AuthLayout = ()=> {
     return (
         <Stack screenOptions={{headerShown: false}}>
-            /*<Stack.Screen name="index" options={{title: "Login"}}/>*/
+            <Stack.Screen name="index" options={{title: "Login"}}/>
             <Stack.Screen name="register" options={{title: "Cadastro"}}/>
             <Stack.Screen name="resetPassword" options={{title: "Esqueci minha senha"}}/>
 
