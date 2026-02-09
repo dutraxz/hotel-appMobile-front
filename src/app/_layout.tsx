3bbd1eefb0f4a20eb5e5fe391ec36d4cbed38048
dutraxz <victoreduardo0896@gmail.com>
2026-02-09 17:31:09 -0300

    feat - adicionando authProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,16 @@
 import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import AuthProvider from "@/contexts/AuthContext";
+
+
 
 const RootLayout = () => {
     {/* Slot atribui ao fluxo de navegação "child" o papel de definir como as telas navegarão
         entre si (ex.: /(auth) ou /(tabs), sem impor uma forma de navegação no nível raiz */}
-        return <Slot/>
-}
+        return (
+            <AuthProvider>
+                <Stack screenOptions={{ headerShown: false }}>
+                    </AuthProvider>
+    );
+};
 export default RootLayout;
